68e634321d295f37224e64e9dabc1987936fde7f
spectrumsloth600 <61101947+spectrumsloth600@users.noreply.github.com>
2020-05-06 18:19:18 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# github-repository-test
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ SlingShot.js
@@ -0,0 +1,24 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.Sling = Constraint.create(options);
+        World.add(world, this.Sling);
+    }
+fly(){
+    this.Sling.bodyA=null;
+}
+
+    display(){
+        var pointA = this.Sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    
+}
